7b6ff8d79d5d1b0764c41f94ed445704c30bbcb0
NexVeridian <nexveridian@gmail.com>
2025-01-15 06:23:02 -0800

    add openapi to album

--- src/controllers/album.rs
+++ src/controllers/album.rs
@@ -1,16 +1,37 @@
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::unnecessary_struct_initialization)]
 #![allow(clippy::unused_async)]
-use loco_rs::prelude::*;
 use axum::debug_handler;
+use loco_rs::prelude::*;
+use serde::Serialize;
 
+#[derive(Serialize, Debug, ToSchema)]
+pub struct Album {
+    title: String,
+    rating: u32,
+}
+
+/// Get album
+///
+/// Returns a title and rating
+#[utoipa::path(
+    get,
+    path = "/api/album/get_album",
+    tags = ["album"],
+    responses(
+        (status = 200, description = "Album found", body = Album),
+    ),
+)]
 #[debug_handler]
-pub async fn index(State(_ctx): State<AppContext>) -> Result<Response> {
-    format::empty()
+pub async fn get_album(State(_ctx): State<AppContext>) -> Result<Response> {
+    format::json(Album {
+        title: "VH II".to_string(),
+        rating: 10,
+    })
 }
 
 pub fn routes() -> Routes {
     Routes::new()
-        .prefix("api/albums/")
-        .add("/", get(index))
+        .prefix("api/album/")
+        .add("/get_album", routes!(get_album))
 }
