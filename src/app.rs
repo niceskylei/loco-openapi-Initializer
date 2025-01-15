862a8487cedda8dc0a21e191724599c3a5c7b2fe
NexVeridian <nexveridian@gmail.com>
2025-01-15 06:24:08 -0800

    scaffold album

--- src/app.rs
+++ src/app.rs
@@ -49,6 +49,7 @@ impl Hooks for App {
 
     fn routes(_ctx: &AppContext) -> AppRoutes {
         AppRoutes::with_default_routes() // controller routes below
+            .add_route(controllers::album::routes())
             .add_route(controllers::auth::routes())
     }
     async fn connect_workers(ctx: &AppContext, queue: &Queue) -> Result<()> {
